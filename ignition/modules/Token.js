70ef958f11825e1aa70af9a4c10d9ddf57e958b2
flutchidev <scottdavis8188@gmail.com>
2025-06-11 22:37:15 +0900

    restructure the files

--- ignition/modules/Token.js
+++
@@ -1,8 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-const DeployModule = buildModule("TokenModule", (m) => {
-  const nftStore = m.contract("NFTSTORE");  // Ensure contract name matches exactly
-  return {nftStore};
-});
-
-module.exports = DeployModule;
